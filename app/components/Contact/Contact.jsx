b47ca3747ca4baa8cb90d369c3f2ce3552590c10
Denise <deniselucianarosado@gmail.com>
2023-08-18 03:19:00 -0300

    .

--- app/components/Contact/Contact.jsx
+++ app/components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './contact.module.css'
 
+
 export default function Contact() {
   return (
     <div className={styles.contactContainer} id="contact">
--- app/components/welcome/Welcome.jsx
+++ app/components/welcome/Welcome.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import styles from './welcome.module.css'
 import Image from 'next/image'
 import profilephoto from '../../../public/profilephoto.png'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import manitoholi from '../../../public/manitoholi.png'
+config.autoAddCss = false
 
 export default function Welcome() {
   return (
@@ -10,15 +14,45 @@ export default function Welcome() {
 
         <div className={styles.descriptionContainer}>
 
-            <div className={styles.leftContainer}>
-                <p>Hi! I&apos;m</p>
-       <h1 className={styles.welcomeTitle}>Denise Rosado</h1> 
-       <h2>A FullStack Developer based in Argentina</h2>
+        <div className={styles.leftContainer}>
+
+
+        <div className={styles.textContainer}>
+        <h2 className={styles.welcomeSubtitle}>
+          Hi! <Image src={manitoholi} className={styles.manito} width={100}/> I&apos;m  
+        <span className={styles.myName}> Denise Rosado
+        </span></h2>
+        
+
+
+       <h1 className={styles.welcomeTitle}>FullStack Developer</h1> 
+       <h2 className={styles.welcomeSubtitle}>based in Buenos Aires, Argentina.</h2>
+
+      </div>
+
+       
+      <div className={styles.buttonContainer}>
+       <a href='/drosadocv.pdf' 
+       download='drosadocv'>
+        <button
+        className={styles.downloadButton}>Resume</button>
+      </a>
+      </div>
+
+
+
        </div>
+
+
+
+
 
        <div className={styles.rightContainer}>
        <div className={styles.imageContainer}>
        <Image src={profilephoto} alt="Profile Photo" layout="responsive" className={styles.image}/>
+
+
+       
        </div>
        </div>
 
