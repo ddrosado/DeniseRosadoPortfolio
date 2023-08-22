3d19365625f4ef9c0d80bc111fb413d4594ee1d0
Denise <deniselucianarosado@gmail.com>
2023-08-22 10:10:42 -0300

    .

--- app/components/stack/Stack.jsx
+++ app/components/stack/Stack.jsx
@@ -4,13 +4,21 @@ import styles from './stack.module.css'
 export default function Stack() {
   return (
     <div className={styles.stackContainer}>
-       <div className={styles.frontContainer}></div>
+       <div className={styles.frontContainer}>
+        Front-End
+       </div>
 
-       <div className={styles.backContainer}></div>
+       <div className={styles.backContainer}>
+        Back-End
+       </div>
 
-       <div className={styles.otherContainer}></div>
+       <div className={styles.otherContainer}>
+        Ohter
+       </div>
 
-       <div className={styles.softContainer}></div>
+       <div className={styles.softContainer}>
+        Soft skills
+       </div>
     </div>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import Welcome from './components/welcome/Welcome'
 import Nav from './components/nav/Nav'
 import Projects from './components/projects/Projects'
 import About from './components/aboutme/About'
-import Opinions from './opinions/Opinions'
-import Contact from './components/Contact/Contact'
+import Opinions from './components/opinions/Opinions'
+import Contact from './components/contact/Contact'
 
 
 export default function Home() {
