d760f7d2038c191238b67be379c5c899b5387882
Denise <deniselucianarosado@gmail.com>
2023-08-29 18:39:55 -0300

    minor changes

--- app/components/aboutme/About.jsx
+++ app/components/aboutme/About.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import styles from './about.module.css'
-import Stack from '../stack/Stack'
+
 
 export default function About() {
   return (
     <div className="section">
     <div className={styles.aboutContainer} id="about">
 
-    {/* <div className={styles.descriptionContainer}>
-      <h1 className={styles.aboutTitle}>
-        <span># </span>
-        About me
-        </h1>
-
-
-    <div className={styles.descriptionText}>
-    <h3 className={styles.aboutText}>
-    I enjoy working in a collaborative environment that encourages continuous learning. I easily adapt to new contexts, and I believe that effective communication and collaboration are key to achieving goals. I'm always ready to contribute ideas, and working together with a team enhances my productivity. Hope we can work together soon!
-    </h3>
-    </div>
-    </div> */}
-
+    <div className={styles.topContainer}></div>
+    <div className={styles.bottomContainer}></div>
 
 
     </div>
     </div>
   )
 }
+
+{/* <div className={styles.descriptionContainer}>
+  <h1 className={styles.aboutTitle}>
+    <span># </span>
+    About me
+    </h1>
+
+
+<div className={styles.descriptionText}>
+<h3 className={styles.aboutText}>
+I enjoy working in a collaborative environment that encourages continuous learning. I easily adapt to new contexts, and I believe that effective communication and collaboration are key to achieving goals. I'm always ready to contribute ideas, and working together with a team enhances my productivity. Hope we can work together soon!
+</h3>
+</div>
+</div> */}
