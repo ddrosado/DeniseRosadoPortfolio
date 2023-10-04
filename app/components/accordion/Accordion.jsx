17f477249d285768bcb191ed3c71cacb422b15f0
Denise <deniselucianarosado@gmail.com>
2023-10-04 00:55:01 -0300

    responsive advances

--- app/components/accordion/Accordion.jsx
+++ app/components/accordion/Accordion.jsx
@@ -16,10 +16,10 @@ export default function Accordion() {
     <div>
         <div id="accordionExample">
   <div
-    class="rounded-t-lg border border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
-    <h2 class="mb-0" id="headingOne">
+    className="rounded-t-lg border border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+    <h2 className="mb-0" id="headingOne">
       <button
-        class="group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
+        className="group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
         type="button"
         data-te-collapse-init
         data-te-target="#collapseOne"
@@ -34,7 +34,7 @@ export default function Accordion() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="h-6 w-6">
+            className="h-6 w-6">
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -45,22 +45,21 @@ export default function Accordion() {
     </h2>
     <div
       id="collapseOne"
-      class="!visible"
+      className="!visible"
       data-te-collapse-item
       data-te-collapse-show
       aria-labelledby="headingOne"
       data-te-parent="#accordionExample">
       <div class="px-5 py-4">
-        Todavía estoy construyendo mi portafolio. En el mismo estoy utilizando Next.js, React, Tailwind, CSS y varias librerías de componentes visuales. También comence un pequeño proyecto para aprender Typescript y React Query.
-        {/* Estoy haciendo un proyecto fullstack, un directorio el cual me facilita acceder a recursos visuales que me gustaron anteriormente. Las tecnologías que utilizo en este proyecto son React, React Query, Next.js, Typescript, C# .NET, y PostgreSQL. */}
+        Todavía estoy construyendo mi portafolio. En el mismo estoy utilizando Next.js, React, Tailwind, CSS y varias librerías de componentes visuales. 
       </div>
     </div>
   </div>
   <div
     class="border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
-    <h2 class="mb-0" id="headingTwo">
+    <h2 className="mb-0" id="headingTwo">
       <button
-        class="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
+        className="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
         type="button"
         data-te-collapse-init
         data-te-collapse-collapsed
@@ -76,7 +75,7 @@ export default function Accordion() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="h-6 w-6">
+            className="h-6 w-6">
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -87,21 +86,21 @@ export default function Accordion() {
     </h2>
     <div
       id="collapseTwo"
-      class="!visible hidden"
+      className="!visible hidden"
       data-te-collapse-item
       aria-labelledby="headingTwo"
       data-te-parent="#accordionExample">
-      <div class="px-5 py-4">
+      <div className="px-5 py-4">
         Avanzado, C2 en lectoescritura y C1 en conversacional! Hago todas mis búsquedas en inglés, me es muy útil para encontrar soluciones y leer documentación.
       </div>
     </div>
   </div>
 
   <div
-    class="rounded-b-lg border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
-    <h2 class="accordion-header mb-0" id="headingThree">
+    className="rounded-b-lg border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+    <h2 className="accordion-header mb-0" id="headingThree">
       <button
-        class="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
+        className="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
         type="button"
         data-te-collapse-init
         data-te-collapse-collapsed
@@ -110,14 +109,14 @@ export default function Accordion() {
         aria-controls="collapseThree">
         ¿Está terminado tu porfolio?
         <span
-          class="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
+          className="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="h-6 w-6">
+            className="h-6 w-6">
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -128,21 +127,21 @@ export default function Accordion() {
     </h2>
     <div
       id="collapseThree"
-      class="!visible hidden"
+      className="!visible hidden"
       data-te-collapse-item
       aria-labelledby="headingThree"
       data-te-parent="#accordionExample">
-      <div class="px-5 py-4">
+      <div className="px-5 py-4">
         Gracias por visitar mi portfolio! todavía está en construcción.
       </div>
     </div>
   </div>
 
 <div
-    class="rounded-b-lg border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
-    <h2 class="accordion-header mb-0" id="headingFour">
+    className="rounded-b-lg border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+    <h2 className="accordion-header mb-0" id="headingFour">
       <button
-        class="group relative flex w-full items-center border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)] [&[data-te-collapse-collapsed]]:rounded-b-[15px] [&[data-te-collapse-collapsed]]:transition-none"
+        className="group relative flex w-full items-center border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)] [&[data-te-collapse-collapsed]]:rounded-b-[15px] [&[data-te-collapse-collapsed]]:transition-none"
         type="button"
         data-te-collapse-init
         data-te-collapse-collapsed
@@ -151,14 +150,14 @@ export default function Accordion() {
         aria-controls="collapseFour">
         ¿Qué consideras que te distingue?
         <span
-          class="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
+          className="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="h-6 w-6">
+            className="h-6 w-6">
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -169,11 +168,11 @@ export default function Accordion() {
     </h2>
     <div
       id="collapseFour"
-      class="!visible hidden"
+      className="!visible hidden"
       data-te-collapse-item
       aria-labelledby="headingFour"
       data-te-parent="#accordionExample">
-      <div class="px-5 py-4">
+      <div className="px-5 py-4">
         <p className={styles.paragraph}>
           Me comprometo y esfuerzo al máximo en todo lo que hago. La programación es algo que me apasiona y disfruto de cada paso del proceso, tanto aprender como aplicar conocimientos. Tengo un enfoque persistente y, ante un problema, aunque deba buscar en los rincones más recónditos de Internet, siempre utilizo todos los recursos para poder encontrar una solución.
         </p>
