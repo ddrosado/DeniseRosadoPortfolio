4ebc5b11c26d468ac480816f915fc1a23dc0dba0
Denise <deniselucianarosado@gmail.com>
2023-10-19 15:15:52 -0300

    accordeon update

--- app/components/accordion/Accordion.jsx
+++ app/components/accordion/Accordion.jsx
@@ -97,7 +97,7 @@ export default function Accordion() {
   </div>
 
   <div
-    className="rounded-b-lg border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+    className="border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
     <h2 className="accordion-header mb-0" id="headingThree">
       <button
         className="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
@@ -107,7 +107,7 @@ export default function Accordion() {
         data-te-target="#collapseThree"
         aria-expanded="false"
         aria-controls="collapseThree">
-        ¿Está terminado tu porfolio?
+        ¿FrontEnd o BackEnd?
         <span
           className="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
           <svg
@@ -132,7 +132,7 @@ export default function Accordion() {
       aria-labelledby="headingThree"
       data-te-parent="#accordionExample">
       <div className="px-5 py-4">
-        Gracias por visitar mi portfolio! todavía está en construcción.
+        En este momento estoy profundizando en .NET y es, sinceramente, un lenguaje que me atrapó por completo. Aunque me gusta ser capaz de comprender las aplicaciones en su total integridad y disfruto de tomar mates mientras coloreo botones y maqueto interfaces, actualmente me inclino por el backend.
       </div>
     </div>
   </div>
